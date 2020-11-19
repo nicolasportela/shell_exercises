8090463d9023a25e912aa4671f1bd162baa52179
nicolasportela <2103@holbertonschool.com>
2020-11-19 14:12:46 +0000

    final commit

--- 2_3_4-getenv_directories_in_path_and_linked_list_of_path.c
+++ 2_3_4-getenv_directories_in_path_and_linked_list_of_path.c
@@ -3,8 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
+
+
 char *_getenv(const char *name);
+list_t *add_node_end(list_t **head, const char *str);
 void p_path(char *line);
+
 
 int main(void)
 {
@@ -37,11 +46,55 @@ char *_getenv(const char *name)
 void p_path(char *line) /*get the return of _getenv*/
 {
 	char *ch;
+	list_t *head = NULL;
 
+	if (line == NULL)
+	{
+		return;
+	}
 	ch = strtok(line, ":"); /*tokenize the value of PATH*/
-	while (ch != 0)
+	while (ch != NULL)
 	{
 		printf("%s\n", ch); /*print each token*/
+		add_node_end(&head, ch);
+		printf("node created");
 		ch = strtok(NULL, ":"); /*move the token to next*/
 	}
+}
+
+/* create node*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode, *temp;
+
+	temp = *head;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	{
+		return (0);
+	}
+
+	(*newnode).str = strdup(str);
+	(*newnode).next = NULL;
+
+	if (*head == NULL)
+	{
+		(*head) = newnode;
+		return (newnode);
+	}
+
+	while ((*temp).next)
+	{
+		temp = (*temp).next;
+	}
+
+	(*temp).next = newnode;
+	return (newnode);
 }
